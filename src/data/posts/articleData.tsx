6c811cbe4f19aef1c3a585e84523c08e5c8d51b7
svenbrodny <sven.brodny@piobyte.de>
2020-06-03 19:14:08 +0200

    fix: missing lf-6 posts in "artilcleData" variable

--- src/data/posts/articleData.tsx
+++ src/data/posts/articleData.tsx
@@ -4,7 +4,8 @@ import {lf2_posts} from "./lf-2";
 import {lf3_posts} from "./lf-3";
 import {lf4_1_posts} from "./lf-4-1";
 import {lf4_2_posts} from "./lf-4-2";
+import {lf6_posts} from "./lf-6";
 
 export const articleData: Post[] = [
-    ...lf1_posts, ...lf2_posts, ...lf3_posts, ...lf4_1_posts, ...lf4_2_posts
+    ...lf1_posts, ...lf2_posts, ...lf3_posts, ...lf4_1_posts, ...lf4_2_posts, ...lf6_posts
 ];
