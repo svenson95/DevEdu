a9422171a814415f3779c9ff36772c711d8b9884
Sven Brodny <sven.brodny@piobyte.de>
2020-08-24 16:57:42 +0200

    feat: updated site (document) title in route components respectively (2)
    - missing updated menuTitles array (new shortTitle prop for document.title value)

--- src/data/menuTitles.tsx
+++ src/data/menuTitles.tsx
@@ -27,89 +27,104 @@ interface appPage {
     iosIcon: string;
     mdIcon: string;
     title: string;
+    shortTitle: string;
 }
 
 export const subjects: appPage[] = [
     {
         title: 'Lernfeld 1',
+        shortTitle: 'LF 1',
         url: '/lf-1',
         iosIcon: earthOutline,
         mdIcon: earthSharp
     },
     {
         title: 'Lernfeld 2',
+        shortTitle: 'LF 2',
         url: '/lf-2',
         iosIcon: businessOutline,
         mdIcon: businessSharp
     },
     {
         title: 'Lernfeld 3',
+        shortTitle: 'LF 3',
         url: '/lf-3',
         iosIcon: pieChartOutline,
         mdIcon: pieChartSharp
     },
     {
         title: 'Lernfeld 4-1',
+        shortTitle: 'LF 4-1',
         url: '/lf-4-1',
         iosIcon: flashOutline,
         mdIcon: flashSharp
     },
     {
         title: 'Lernfeld 4-2',
+        shortTitle: 'LF 4-2',
         url: '/lf-4-2',
         iosIcon: hardwareChipOutline,
         mdIcon: hardwareChipSharp
     },
     {
         title: 'Lernfeld 5',
+        shortTitle: 'LF 5',
         url: '/lf-5',
         iosIcon: codeSlashOutline,
         mdIcon: codeSlashSharp
     },
     {
         title: 'Lernfeld 6',
+        shortTitle: 'LF 6',
         url: '/lf-6',
         iosIcon: codeSlashOutline,
         mdIcon: codeSlashSharp
     },
     {
         title: 'Lernfeld 7',
+        shortTitle: 'LF 7',
         url: '/lf-7',
         iosIcon: gitNetworkOutline,
         mdIcon: gitNetworkSharp
     },
     {
         title: 'Lernfeld 8',
+        shortTitle: 'LF 8',
         url: '/lf-8',
         iosIcon: swapHorizontalOutline,
         mdIcon: swapHorizontalSharp
     },
     {
         title: 'Lernfeld 9',
+        shortTitle: 'LF 9',
         url: '/lf-9',
         iosIcon: globeOutline,
         mdIcon: globeSharp
     },
     {
         title: 'Wahlpflicht',
+        shortTitle: 'WP',
         url: '/wp',
         iosIcon: serverOutline,
         mdIcon: serverSharp
     },
     {
         title: 'WiSo',
+        shortTitle: 'WiSo',
         url: '/wiso',
         iosIcon: bodyOutline,
         mdIcon: bodySharp
     },
     {
         title: 'Englisch',
+        shortTitle: 'Eng',
         url: '/englisch',
         iosIcon: languageOutline,
         mdIcon: languageSharp
     },
     {
         title: 'Deutsch',
+        shortTitle: 'Deu',
         url: '/deutsch',
         iosIcon: pencilOutline,
         mdIcon: pencilSharp
@@ -119,30 +134,35 @@ export const subjects: appPage[] = [
 export const areas: appPage[] = [
     {
         title: 'Syntax',
+        shortTitle: 'Syntax',
         url: '/syntax',
         iosIcon: codeSlashOutline,
         mdIcon: codeSlashSharp
     },
     {
         title: 'Angewandte Informatik',
+        shortTitle: 'Ang. Inf.',
         url: '/angewandte-informatik',
         iosIcon: libraryOutline,
         mdIcon: librarySharp
     },
     {
         title: 'Technische Informatik',
+        shortTitle: 'Tech. Inf.',
         url: '/technische-informatik',
         iosIcon: libraryOutline,
         mdIcon: librarySharp
     },
     {
         title: 'Praktische Informatik',
+        shortTitle: 'Prak. Inf.',
         url: '/praktische-informatik',
         iosIcon: libraryOutline,
         mdIcon: librarySharp
     },
     {
         title: 'Theoretische Informatik',
+        shortTitle: 'Theo. Inf.',
         url: '/theoretische-informatik',
         iosIcon: libraryOutline,
         mdIcon: librarySharp
@@ -152,42 +172,49 @@ export const areas: appPage[] = [
 export const internal: appPage[] = [
     {
         title: 'Lehrmaterial',
+        shortTitle: 'Lehrmaterial',
         url: '/lehrmaterial',
         iosIcon: folderOutline,
         mdIcon: folderSharp
     },
     {
         title: 'Mitteilungen',
+        shortTitle: 'Mitteilungen',
         url: '/mitteilungen',
         iosIcon: mailOutline,
         mdIcon: mailSharp
     },
     {
         title: 'Schuljahresplan',
+        shortTitle: 'Schuljahresplan',
         url: '/schuljahresplan',
         iosIcon: schoolOutline,
         mdIcon: schoolSharp
     },
     {
         title: 'Vertretungsplan',
+        shortTitle: 'Vertretungsplan',
         url: '/vertretungsplan',
         iosIcon: calendarOutline,
         mdIcon: calendarSharp
     },
     {
         title: 'Lehrer',
+        shortTitle: 'Lehrer',
         url: '/lehrer',
         iosIcon: peopleOutline,
         mdIcon: peopleSharp
     },
     {
         title: 'Feedback',
+        shortTitle: 'Feedback',
         url: '/feedback',
         iosIcon: megaphoneOutline,
         mdIcon: megaphoneSharp
     },
     {
         title: 'Klausuren',
+        shortTitle: 'Klausuren',
         url: '/klausuren',
         iosIcon: todayOutline,
         mdIcon: todaySharp
