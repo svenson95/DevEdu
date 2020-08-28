22ad16e37052e513358b16ce88184eedf7bfd5dd
Sven Brodny <sven.brodny@piobyte.de>
2020-08-29 00:44:10 +0200

    feat: app spinner - during initial isAuthenticated request

--- src/app/components/Spinner.tsx
+++ src/app/components/Spinner.tsx
@@ -1,7 +1,7 @@
 import {IonSpinner} from "@ionic/react";
 import React from "react";
 
-export const LoadingSpinner = () =>
-    <div className="spinner__wrapper">
+export const LoadingSpinner = ({ ...props }) =>
+    <div className={props.appLoader ? "spinner__wrapper app-loader" : "spinner__wrapper"}>
         <IonSpinner name="crescent"/>
     </div>;
