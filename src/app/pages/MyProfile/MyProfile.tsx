e16c05cdf1fb6391c8d3adf8b1129e3b203b367b
Sven Brodny <sven.brodny@piobyte.de>
2020-09-05 10:37:18 +0200

    feat: update user-theme option in my-profile component

--- src/app/pages/MyProfile/MyProfile.tsx
+++ src/app/pages/MyProfile/MyProfile.tsx
@@ -1,9 +1,11 @@
 import React, {useContext, useEffect, useState} from "react";
 import {
+    IonBadge,
     IonButton,
     IonCard,
     IonContent,
     IonPage,
+    IonToggle,
 } from "@ionic/react";
 import './MyProfile.scss';
 
@@ -12,10 +14,12 @@ import {ErrorContext, SearchPostContext} from "../../components/split-pane/Conte
 import PopoverEditUser from "../../components/Popover-EditUser/Popover-EditUser";
 import DataService from "../../services/data.service";
 import {LoadContext} from "../../../App";
+import AuthService from "../../services/auth.service";
 
 const MyProfile = () => {
     const [showPopover, setShowPopover] = useState(false);
     const [progressPercentage, setProgressPercentage] = useState(null as any);
+    const [toggleValue, setToggleValue] = useState("");
     const loadContext = useContext(LoadContext);
     const authContext = useContext(AuthContext);
     const errorContext = useContext(ErrorContext);
@@ -28,14 +32,34 @@ const MyProfile = () => {
     };
 
     useEffect(() => {
+        setToggleValue(authContext?.user.theme);
+
         loadContext.setLoading(true);
         DataService.getAllLessons()
             .then(postsArray => {
                 setProgressPercentage(Math.round((authContext?.user.progress.length / postsArray.length * 100) * 10) / 10);
             })
             .catch(error => errorContext.setMessage(error))
-            .finally(() => loadContext.setLoading(false))
+            .finally(() => loadContext.setLoading(false));
     }, []);
+
+    function saveNewTheme() {
+        let updatedUser = {
+            "_id": authContext.user._id,
+            "name": authContext.user.name,
+            "email": authContext.user.email,
+            "password": authContext.user.password,
+            "progress": authContext.user.progress,
+            "role": authContext.user.role,
+            "theme": toggleValue
+            // "password": (oldPassword !== null && newPassword !== null) ? newPassword : props.user?.password
+        };
+        loadContext.setLoading(true);
+        AuthService.editUser(updatedUser)
+            .then(res => errorContext.setMessage("Neues Standard-Theme gespeichert"))
+            .catch(error => console.log(error))
+            .finally(() => loadContext.setLoading(false));
+    }
 
     return (
         <IonPage id="main">
@@ -65,6 +89,17 @@ const MyProfile = () => {
                         <div className="content-row">
                             <p className="key" id="progress-key">Fortschritt</p>
                             <p className="value" id="progress-val">{progressPercentage !== null ? progressPercentage : "..."}%</p>
+                        </div>
+                        <div className="content-row">
+                            <p className="key" id="progress-key">Standard-Theme</p>
+                            <p className="value" id="progress-val">
+                                <span className="light-label">Light</span>
+                                <IonToggle checked={toggleValue === "dark"} onIonChange={() => {
+                                    toggleValue === "dark" ? setToggleValue("light") : setToggleValue("dark");
+                                }}/>
+                                <span className="dark-label">Dark</span>
+                                {authContext.user.theme !== toggleValue && <IonBadge onClick={saveNewTheme}>SPEICHERN</IonBadge>}
+                            </p>
                         </div>
                     </div>
                     <div className="buttons">
